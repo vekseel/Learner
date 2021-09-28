49571dcf8e9c237dd79c08594c8d3cf160eeed05
vekseel <alexeev.aleksandr@inbox.ru>
2021-09-28 20:28:55 +0300

    added all files

---
+++ mobile-app/mobile-app/mobile-app/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using mobile_app.ViewModels;
+using mobile_app.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace mobile_app
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
