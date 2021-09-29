7d073c7b50b26017e181d36023796ae4a6b6d296
vekseel <alexeev.aleksandr@inbox.ru>
2021-09-29 13:33:02 +0300

    working with authenification

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
-@Module({})
+@Module({
+  imports: [AuthModule, UsersModule]
+})
 export class AppModule {}
