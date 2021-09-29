3648a5ed36acd35ab1551cf881e5b303051b93f0
vekseel <alexeev.aleksandr@inbox.ru>
2021-09-29 13:23:53 +0300

    working with modules

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,15 @@
-import {NestFactory} from "@nestjs/core";
-import {AppModule} from "./app.moule";
-
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 const start = async () => {
-    try{
-        const app = await NestFactory.create(AppModule)
-    } catch (e){
-        console.log(e)
-    }
-}
+  try {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule);
+
+    await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
